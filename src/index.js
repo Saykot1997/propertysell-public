56d2600a798d95f0d5d9ecaf28b05b36c438695e
Saykot Hossain <saykothossain14@gmail.com>
2022-12-02 13:05:09 +0600

    compretion add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const app = express();
 const dotenv = require('dotenv');
 const db = require("./config/db_connect");
 const cors = require("cors");
+const compression = require('compression');
+const rateLimit = require('express-rate-limit')
 const createError = require('http-errors');
 const { error_handler } = require("./error/error_handler");
 const AdminRouter = require("./routers/admin_router");
@@ -11,7 +13,6 @@ const PropertyRouter = require("./routers/property_router");
 const AgentRouter = require("./routers/agent_router");
 const LocationRouter = require("./routers/location_router")
 
-// console.log(process.env.PORT)
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +21,30 @@ dotenv.config();
 
 // connect to mongodb
 db()
+
+// compretion data before response
+app.use(compression({
+    level: 6,
+    threshold: 100 * 1000,
+    filter: (req, res) => {
+        if (req.headers["x-on-compression"]) {
+            return false
+        }
+        return compression.filter(req, res)
+    }
+}))
+
+// rate limit
+app.use(rateLimit({
+    windowMs: 5000, // 5 secound
+    max: 5, // Limit each IP to 5 requests per `window` (here, per 5 secound)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: {
+        code: 429,
+        message: "Too many request."
+    }
+}))
 
 app.get("/api/", (req, res) => {
     res.send("welcome to global immigration consultant property sell api");
